ef2bff8f4dbb466f7c8ac851d577fd1444d37779
Matthew Anderson <matt.anderson.inf@gmail.com>
2022-09-24 14:12:38 -0400

    gitignore edited tto hide testing config file

---
+++ testing/Config/config.py
@@ -0,0 +1,10 @@
+
+
+class Config:
+    CHROME_DRIVER_EXECUTABLE_PATH = "../Drivers/chromedriver.exe"
+    FIREFOX_DRIVER_EXECUTABLE_PATH = "../Drivers/geckodriver.exe"
+    EDGE_RIVER_EXECUTABLE_PATH = "../Drivers/msedgedriver.exe"
+
+    BASE_URL = "https://morning-savannah-46253.herokuapp.com/login"
+    USERNAME = ""
+    PASSWORD = ""
