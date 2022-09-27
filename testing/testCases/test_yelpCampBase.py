f852f0cd07b2741a79a2038b68014fe7da549f86
Matthew Anderson <matt.anderson.inf@gmail.com>
2022-09-27 00:33:35 -0400

    now i need to connecto the driver to the testcase

--- testing/testCases/test_yelpCampBase.py
+++ testing/testCases/test_yelpCampBase.py
@@ -7,8 +7,5 @@ from Pages.yelpCampBasePage import yelpCampBasePage
 @pytest.mark.usefixtures("init_driver")
 class yelpCampBaseTest(yelpCampBasePage):
 
-    """ constructor of the page class """
-    def __init__(self, driver):
-        
-        super().__init__(driver)
+    pass
 
